ad6244d6483487c9a64f6152ed8c43d7adf2832b
Prince Shaji <22243569+PrinceShaji@users.noreply.github.com>
2019-05-06 23:43:34 +0530

    Created the move class.

--- adventure.py
+++ adventure.py
@@ -1,1 +1,46 @@
 # Adventure game
+
+
+#The board or the list that represent the board.
+board = []
+#The size of the board. It's set to zero as default.
+board_length = 0
+#This stores the current coordinates of the user. It is 0,0 by default.
+user_location = ['x','y']
+
+
+
+
+class move:
+
+	def up():
+		'''
+		since the user is only moving upward, 
+		we only have to consider the y coordinate
+		cause x is constant
+		'''
+
+
+
+
+	def down():
+		'''
+		x is constant, y is svariable
+		'''
+
+
+	def right():
+		'''
+		since the user is only moving right, 
+		we only have to consider the x coordinate
+		cause y is constant
+		'''
+
+
+
+	def left():
+		'''
+		y is constant, x is variable
+		'''
+
+	
