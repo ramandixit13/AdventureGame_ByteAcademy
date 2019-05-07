1d0919e5fa7b95d94a72a90d0095b23b0dd62c4b
Prince Shaji <22243569+PrinceShaji@users.noreply.github.com>
2019-05-07 08:59:29 +0530

    Added the class result, (check and show functions)

--- adventure.py
+++ adventure.py
@@ -4,15 +4,24 @@
 #The board or the list that represent the board.
 board = []
 '''
-board = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]] >> blank board
+board = [[1,0,0,0],[0,1,0,0],[0,1,0,0],[2,1,1,1]] >> blank board
 board[y] is the y coordinate.
 board[y][x] is the x coordinate.
+
+0[1,0,0,0]
+1[0,1,0,0]
+2[0,1,0,0]
+3[2,1,1,1]
+  0 1 2 3
+
 '''
 #The size of the board. It's set to zero as default.
 grid_size = 4
 board_size = grid_size - 1
+
 #This stores the current coordinates of the user. It is 0,0 by default.
-user_location = ['x','y']
+#user_location = ['x', 'y'] >> Syntax
+user_location = [0,0]
 x = user_location[0]
 y = user_location[1]
 
@@ -26,23 +35,28 @@ class move:
 		since the user is only moving upward, 
 		we only have to consider the y coordinate
 		cause x is constant
-		'''
+		'''	
 		if y <= board_size and y != o:
 			if board[y-1][x] == 0:
 				#print("Cannot move outside the path. Make another choice.")
-			elif board[y-1][x] == 1:
+			elif(board[y-1][x] == 1):
+
 				#print("user moved up")
 				user_location[1] -= 1
+			
 			elif board[y-1][x] == 2:
 				#print("user moved up")
 				user_location[1] -= 1
+		
 		else:
 			print("Sorry! You cannot move outside the board.")
+			move.choice()
 
 	def down():
 		'''
 		x is constant, y is variable
 		'''
+		
 		if y < board_size:
 			if board[y+1][x] == 0:
 				#print("Cannot move outside the path. Make another choice.")
@@ -54,9 +68,10 @@ class move:
 				user_location[1] += 1
 		else:
 			print("Sorry! You cannot move outside the board.")
+			move.choice()
 
 	def right():
-		'''
+		'''		
 		since the user is only moving right, 
 		we only have to consider the x coordinate
 		cause y is constant
@@ -65,6 +80,7 @@ class move:
 			if board[y][x+1] == 0:
 				#print("Cannot move outside the path. Make another choice.")
 			elif board[y][x+1] == 1:
+		
 				#print("user moved right")
 				user_location[0] += 1
 			elif board[y][x+1] == 2:
@@ -72,8 +88,7 @@ class move:
 				user_location[0] += 1
 		else:
 			print("Sorry! You cannot move outside the board.")
-
-
+			move.choice()
 
 	def left():
 		'''
@@ -90,6 +105,7 @@ class move:
 				user_location[0] -= 1
 		else:
 			print("Sorry! You cannot move outside the board.")
+			move.choice()
 
 	def choice():
 		'''
@@ -111,4 +127,18 @@ class move:
 			print("You have made an invalid move. Please try again.\n")
 			move.choice() #Wrong choices are excluded from the max move counter.
 
-	
+class result:
+
+	def check():
+		if board[y][x] == 2:
+			result.show()
+
+	def show():
+		print("Congratulations! You Won!")
+		break
+		'''
+		#Adding the name  in leaderboard.
+		with open('leaderboard.txt', 'a') as fout:
+			name = input("Enter your name: ").capitalize()
+			fout.write(f"{name} , score: {}")  #Maybe use the step of the for loop to formulate the score?
+		'''
